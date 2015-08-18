4208aaa198b4ab9d11d1e875d166814d13718de5
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-08-18 16:50:30 +0200

    edit view

--- tablesorter/partials/datum.php
+++ tablesorter/partials/datum.php
@@ -2,47 +2,51 @@
 <div class="widget-header header-color-blue">
 <div class="row">
 <div class="col-sm-6">
-<h4 class="bigger lighter">
-	<i class="glyphicon glyphicon-align-justify"></i>&nbsp;
-	DATUM
-</h4>
+	<h4 class="bigger lighter">
+		<i class="glyphicon glyphicon-align-justify"></i>&nbsp;
+		DATUM
+	</h4>
 </div>
 <div class="col-sm-3">
-
-<!--<input type="text" ng-model="filterDatum" class="form-control search header-elements-margin" placeholder="Filter Datum">-->
-<select data-ng-options="datum.DateEntered for datum in datums track by datum.DateEntered" ng-model="filterDatum" ng-change="getUid(filterDatum)" class="form-control search header-elements-margin">
- <option style="display:none" value="">Select ein Datum</option>
-</select>
-
+	<select data-ng-options="datum.DateEntered for datum in datums track by datum.DateEntered" ng-model="filterDatum" ng-change="getUid(filterDatum)" class="form-control search header-elements-margin">
+	 	<option style="display:none" value="">Select ein Datum</option>
+	</select>
 </div>
 </div></div>
-<div class="widget-body ">
-
-<div class="datum">
-	<table border="1">
-	<tr ng-repeat="userid in userids track by userid.UserId| filter : filterDatum">
-			<td><a ng-click="getInfoUid(userid.UserId,userid.DateEntered)">{{userid.UserId}}</a></td>
-			<td>{{userid.Summe}}</td>
-		</tr>
-	</table>
-</div>
-<hr>
-<?php
-if(isset($_GET['userid'])){
-$uid = $_GET['userid'];
-echo $uid;
-}
-?>
-<p><span id="uid"></span></p>
-<div class="userid">
-	<table border="1">
-	<tr ng-repeat="userid2 in userids2">
-			<td>{{userid2.WebsiteId}}</a></td>
-			<td>{{userid2.IpAddress}}</td>
-			<td>{{userid2.Hour}}</td>
-			<td>{{userid2.Summe}}</td>
-		</tr>
-	</table>
-</div>
-</div>
+	<div class="widget-body ">
+		<table>
+			<tr>
+				<td width="30%" style="vertical-align:top;">
+					<div class="datum">
+						<table border="1">
+						<tr ng-repeat="userid in userids track by userid.UserId| filter : filterDatum">
+								<td><a ng-click="getInfoUid(userid.UserId,userid.DateEntered)">{{userid.UserId}}</a></td>
+								<td>{{userid.Summe}}</td>
+							</tr>
+						</table>
+					</div>
+				</td>
+				<td width="30%"></td>
+				<td width="30%">
+					<?php
+					if(isset($_GET['userid'])){
+					$uid = $_GET['userid'];
+					echo $uid;
+					}
+					?>
+					<span id="uid"></span>
+					<div class="userid">
+						<table border="1">
+						<tr ng-repeat="userid2 in userids2">
+								<td>{{userid2.WebsiteId}}</a></td>
+								<td>{{userid2.IpAddress}}</td>
+								<td>{{userid2.Hour}}</td>
+								<td>{{userid2.Summe}}</td>
+							</tr>
+						</table>
+					</div>
+				</td>
+			</tr>
+		</table>
+	</div>
 </div>
